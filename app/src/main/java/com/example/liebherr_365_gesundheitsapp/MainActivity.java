96c9ee57a9852a5c07a9dbcac1af7ea92cf4a538
Otspeck <marvin.otec@hs-furtwangen.de>
2016-11-03 21:34:26 +0100

    Anpassung den Kommentaren

--- app/src/main/java/com/example/liebherr_365_gesundheitsapp/MainActivity.java
+++ app/src/main/java/com/example/liebherr_365_gesundheitsapp/MainActivity.java
@@ -7,10 +7,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //       .-..-..---. .-..-.
+        //       : :; :: .--': :: :
+        //       :    :: `;  : :: :
+        //       : :: :: :   : :; :
+        //       :_;:_;:_;   `.__.'
+        //
+        // This project was developed during our project studies.
+        // Wintersemster 16/17
+        // Bussmann    Jan
+        // Hug         Melissa
+        // Otec        Marvin
+        // Speer       Christopher
+        // Wangler     Niklas
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //Test
-        //test 456
-    //123
 }
