71da3c8cca81e5ba60b2647113760cb0a122e0a4
Otspeck <marvin.otec@hs-furtwangen.de>
2016-11-05 09:07:32 +0100

    -Kommentare überarbeitet -Funktion implementiert (integer Werte zu float) -Ausgabe des Gewichts auf der Konsole -Funktion implementiert (monthinteger Werte zu Monat) -Ausgabe des Datums auf der Konsole

--- app/src/main/java/com/example/liebherr_365_gesundheitsapp/MainActivity.java
+++ app/src/main/java/com/example/liebherr_365_gesundheitsapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
 
     //function saveweight onklick @+id/saveButton
     public void saveweight(View view) {
+
+
+        //Datepicker
+        DatePicker date = (DatePicker) findViewById(R.id.dp);
+        int dayinteger = date.getDayOfMonth();
+        int monthinteger = date.getMonth() + 1;
+        int yearinteger = date.getYear();
+
+
+        //Numberpicker
         NumberPicker integer = (NumberPicker) findViewById(R.id.integer);
         NumberPicker afterkomma = (NumberPicker) findViewById(R.id.afterkomma);
 
@@ -56,8 +67,16 @@ public class MainActivity extends AppCompatActivity {
         // call function integertofloat
         float weight = integertofloat(integervalue, afterkommavalue);
 
-        //display weight on console
+        //display weight and month on console
         Log.d("weight", String.valueOf(weight));
+
+        // call function monthinegertostring
+        String month = monthinegertostring(monthinteger);
+
+        //console out with debug
+        Log.d("day", String.valueOf(dayinteger));
+        Log.d("day", String.valueOf(month));
+        Log.d("day", String.valueOf(yearinteger));
 
         /********************************/
         /*** next Step Save variables ***/
@@ -70,5 +89,37 @@ public class MainActivity extends AppCompatActivity {
         result += (float) integervalue;
         result += ((float) afterkommavalue / 10);
         return result;
+    }
+
+    //function monthinteger to string
+    public String monthinegertostring(int dayinteger) {
+        switch (dayinteger) {
+            case 1:
+                return "Januar";
+            case 2:
+                return "Februar";
+            case 3:
+                return "März";
+            case 4:
+                return "April";
+            case 5:
+                return "Mai";
+            case 6:
+                return "Juni";
+            case 7:
+                return "Juli";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "Oktober";
+            case 11:
+                return "November";
+            case 12:
+                return "Dezember";
+
+        }
+        return "a";
     }
 }
