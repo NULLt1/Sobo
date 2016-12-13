68d371a803e59570c60a83a82a7f771139cfdc80
Otspeck <marvin.otec@hs-furtwangen.de>
2016-12-13 16:02:54 +0100

    -modul_weight.xml

--- app/src/main/java/com/example/liebherr_365_gesundheitsapp/ModulWeight.java
+++ app/src/main/java/com/example/liebherr_365_gesundheitsapp/ModulWeight.java
@@ -7,7 +7,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
-import android.widget.TextView;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
@@ -45,7 +43,7 @@ public class ModulWeight extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.modul_weight);
 
         //Set date Button with current date
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY);
