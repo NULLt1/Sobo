bb583f6f755762df52152ec30fc692e773830898
Otspeck <marvin.otec@hs-furtwangen.de>
2016-12-21 08:49:48 +0100

    -Diverse Anpassungen bezüglich der neuen Datenbank                    -List View entfernt

---
+++ app/src/main/java/Database/DBHelperDataSourceModules.java
@@ -0,0 +1,41 @@
+package Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class DBHelperDataSourceModules {
+    private static final String LOG_TAG = DBHelperDataSourceData.class.getSimpleName();
+
+    private SQLiteDatabase databaseModules;
+    private DBHelperModules dbHelperModules;
+
+    public DBHelperDataSourceModules(Context context) {
+        Log.d(LOG_TAG, "<MODULES>Unsere DataSource erzeugt jetzt den dbHelper.<MODULES>");
+        dbHelperModules = new DBHelperModules(context);
+
+    }
+
+    public void open() {
+        Log.d(LOG_TAG, "<MODULES>Eine Referenz auf die Datenbank wird jetzt angefragt.<MODULES>");
+        databaseModules = dbHelperModules.getWritableDatabase();
+        Log.d(LOG_TAG, "<MODULES>Datenbank-Referenz erhalten. Pfad zur Datenbank: " + databaseModules.getPath() + "<MODULES>");
+    }
+
+    public void close() {
+        dbHelperModules.close();
+        Log.d(LOG_TAG, "<MODULES>Datenbank mit Hilfe des DbHelpers geschlossen.<MODULES>");
+    }
+
+    public void deletedb() {
+        databaseModules = dbHelperModules.getWritableDatabase();
+        databaseModules.delete(ModulesQuery.getDbName(), null, null);
+        dbHelperModules.close();
+    }
+
+    //function changemodulstatus
+    public void changemodulstatus() {
+
+    }
+}
