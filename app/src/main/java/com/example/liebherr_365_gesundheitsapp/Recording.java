dbbe841180291889b89eea9c48988ccccceedacd
Stan1904 <niklas.wangler@hs-furtwangen.de>
2017-01-16 18:37:56 +0100

    Button "Gewicht eintragen" im Erfassungsbogen, leitet ins Gewichtsmodul weiter

--- app/src/main/java/com/example/liebherr_365_gesundheitsapp/Recording.java
+++ app/src/main/java/com/example/liebherr_365_gesundheitsapp/Recording.java
@@ -1,7 +1,10 @@
 package com.example.liebherr_365_gesundheitsapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Niklas on 12.01.2017.
@@ -9,10 +12,23 @@ import android.os.Bundle;
 
 public class Recording extends Activity{
 
+    Button weight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recording);
 
+        weight = (Button) findViewById(R.id.a);
+
+        weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent weight = new Intent(Recording.this,ModulWeight.class);
+                startActivity(weight);
+            }
+        });
+
     }
+
+
 }
