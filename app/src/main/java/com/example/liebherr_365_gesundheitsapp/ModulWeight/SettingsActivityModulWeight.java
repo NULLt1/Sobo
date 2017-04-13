dddd834361b91a58ffd96a2feb19aa6a7a0399e1
Otspeck <marvin.otec@hs-furtwangen.de>
2017-04-13 16:07:10 +0200

    -Added upnavigation to settingsactivitymodulweight

--- app/src/main/java/com/example/liebherr_365_gesundheitsapp/ModulWeight/SettingsActivityModulWeight.java
+++ app/src/main/java/com/example/liebherr_365_gesundheitsapp/ModulWeight/SettingsActivityModulWeight.java
@@ -7,7 +7,10 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.renderscript.Float2;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.liebherr_365_gesundheitsapp.AppCompatPreferenceActivity;
 import com.example.liebherr_365_gesundheitsapp.ModulWeight.BmiCalculator;
@@ -29,6 +32,7 @@ public class SettingsActivityModulWeight extends AppCompatPreferenceActivity imp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
         SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
@@ -40,6 +44,16 @@ public class SettingsActivityModulWeight extends AppCompatPreferenceActivity imp
         editTextPref.setSummary(sp.getString("age", null) + " Jahre");
         editTextPref = (EditTextPreference) findPreference("weightgoal");
         editTextPref.setSummary(sp.getString("weightgoal", null) + " kg");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)`enter code here`
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
