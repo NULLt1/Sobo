a3522253813deabab2408decc4f66603e9605110
MeliHug <melissa.hug@hs-furtwangen.de>
2017-05-30 20:01:12 +0200

    -added ModulHealth

--- app/src/main/java/com/example/liebherr_365_gesundheitsapp/ModulHealth/ModulHealth.java
+++ app/src/main/java/com/example/liebherr_365_gesundheitsapp/ModulHealth/ModulHealth.java
@@ -3,10 +3,12 @@ package com.example.liebherr_365_gesundheitsapp.ModulHealth;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+
 import com.example.liebherr_365_gesundheitsapp.R;
 import com.example.liebherr_365_gesundheitsapp.viewAdapter.ListViewAdapterModuleHealth;
 
 public class ModulHealth extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
